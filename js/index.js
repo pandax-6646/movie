84291010aad2fa2e198bbfff15d5a8105b560d84
Panda-X6646 <xzq664673913@163.com>
2021-08-27 14:09:46 +0800

    xxx

--- js/index.js
+++ js/index.js
@@ -1,29 +1,29 @@
 var homeView = {
   init() {
-    this.str = '';
+    this.str = "";
     this.getHomeData();
   },
 
   async getHomeData() {
     var self = this;
 
-    await utils.getData('film/getList', {}, 'get')
+    await utils
+      .getData("film/getList", {}, "get")
       .then(function (res) {
         self.randerMovieData(res.films);
         self.randerSwiperData(res.banners);
-
-      }).catch(function (err) {
-        alert(err);
       })
+      .catch(function (err) {
+        alert(err);
+      });
   },
-
 
   // 渲染电影列表
   randerMovieData(data) {
-
-    var $ul = $('.container').find('ul');
+    var $ul = $(".container").find("ul");
     data.forEach((value) => {
       this.str += `
+
         <li class="wp-100 fs-14">
           <a href="./detail.html?filmId=${value.filmId}" class="flex jc-sb">
             <img src="${value.poster}" alt="">
@@ -34,49 +34,47 @@ var homeView = {
               </p>
               <p class="mb-10">
                 <span>观众评分</span>
-                <span class="score">${value.grade ? Number(value.grade).toFixed(1) : '6.0'}</span>
+                <span class="score">${
+                  value.grade ? Number(value.grade).toFixed(1) : "6.0"
+                }</span>
               </p>
               <p class="mb-10">主演：${value.actorStr}</p>
               <p>${value.nation} | ${value.runtime}分钟</p>
             </div>
             <span class="buy">购票</span>
           </a>
-        </li>`
-    })
+        </li>`;
+    });
 
     $ul.html(this.str);
-
   },
-
 
   // 轮播图插件
   swiperPlugin() {
-    new Swiper('.swiper-container', {
-
+    new Swiper(".swiper-container", {
       // 循环模式选项
       loop: true,
       autoplay: true,
 
       // 如果需要分页器
       pagination: {
-        el: '.swiper-pagination',
-      }
-    })
+        el: ".swiper-pagination",
+      },
+    });
   },
-
 
   // 渲染轮播图数据
   randerSwiperData(data) {
-    var str = '';
-    var $swiper = $('.swiper');
+    var str = "";
+    var $swiper = $(".swiper-wrapper");
 
     data.forEach((value) => {
       if (value.url) {
         str += `<div class="swiper-slide wp-100">
-                <img src="${value.url}" alt="">
-              </div>`
+                  <img src="${value.url}" alt="">
+                </div>`;
       }
-    })
+    });
 
     $swiper.html(str);
 
@@ -85,60 +83,57 @@ var homeView = {
     this.swiperPlugin();
   },
 
-
   // 上拉获取数据
   getPullData() {
-
     // 到达底部的判断： 滚动条滚动的距离 + 视口高度 = 元素底部的相对于文档的距离
     var H = window.innerHeight;
-    var $Container = $('.container');
-    var $NavBottomBar = $('.nav-bottom-bar');
-    var $GetPullData = $('.get-pull-data');
+    var $Container = $(".container");
+    var $NavBottomBar = $(".nav-bottom-bar");
+    var $GetPullData = $(".get-pull-data");
 
-    var containerBot = parseInt($Container.css('height')) + $Container.offset().top;
-    var navBottomH = parseInt($NavBottomBar.css('height'));
+    var containerBot =
+      parseInt($Container.css("height")) + $Container.offset().top;
+    var navBottomH = parseInt($NavBottomBar.css("height"));
     var lock = true;
     var page = 2;
 
     var self = this;
 
-    $GetPullData.text('正在拼命加载中 👻👻👻');
-
+    $GetPullData.text("正在拼命加载中 👻👻👻");
 
     // 全局滚动事件
     window.onscroll = async function () {
       var scrollY = Math.ceil(window.pageYOffset);
 
       // 重新获取元素底部的相对于文档的距离
-      containerBot = parseInt($Container.css('height')) + $Container.offset().top;
-      if ((scrollY + H - navBottomH) - containerBot >= 10 && lock) {
-
+      containerBot =
+        parseInt($Container.css("height")) + $Container.offset().top;
+      if (scrollY + H - navBottomH - containerBot >= 10 && lock) {
         lock = false;
 
         var pageNum = {
           pageNum: page,
         };
 
-        await utils.getData('film/getList', pageNum, 'get')
+        await utils
+          .getData("film/getList", pageNum, "get")
           .then(function (res) {
             if (res.films.length != 0) {
               self.randerMovieData(res.films);
 
               lock = true;
               page++;
-
             } else if (res.films.length == 0) {
-              $GetPullData.text('没有更多了数据 😨😨😨');
+              $GetPullData.text("没有更多了数据 😨😨😨");
               window.onscroll = null;
             }
-
-          }).catch(function (err) {
+          })
+          .catch(function (err) {
             console.log(err);
           });
       }
-    }
-  }
-}
-
+    };
+  },
+};
 
 homeView.init();
